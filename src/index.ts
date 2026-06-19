07517007741e40f923990e64df899d0e7f5760cf
Leonardo2696 <leonardoastargo@gmail.com>
2026-06-18 23:05:43 -0300

    Arreglo addProducts y agrego método estático findProductsBelow

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,20 @@ class Product {
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
+    this.id = Math.floor(Math.random() * 1000);
   }
   id: number;
   name: string;
   price: number;
+  static findProductsBelow(price: number): Product[] {
+    const productsBelow: Product[] = [];
+    for (const product of products) {
+      if (product.price < price) {
+        productsBelow.push(product);
+      }
+    }
+    return productsBelow;
+  }
 }
 
 class User {
@@ -20,9 +30,9 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    // esto no funciona:
-    this.products.push(newProducts);
-    // pista: push no suma muchos items (agrega de a uno)
+    for (const product of newProducts) {
+      this.products.push(product);
+    }
   }
 }
 
